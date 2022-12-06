e3457a3b11485ab647f5a068866e54b9beec180f
Shriram1603 <114002708+Shriram1603@users.noreply.github.com>
2022-12-06 22:27:09 +0530

    switch

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/header/Header';
 import About from './pages/about/About';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
     <Router>
       <Navbar/>
-      <Routes>
+      <Switch>
      <Route>
       <Header/>
       </Route>
       <Route>
       <About/>
       </Route>
-      </Routes>
+      </Switch>
     </Router>
   );
 }
