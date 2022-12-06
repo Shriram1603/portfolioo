13fc158c7bb1985442a8df1b4e1f8a97249eb36b
Shriram1603 <114002708+Shriram1603@users.noreply.github.com>
2022-12-06 22:08:42 +0530

    about completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/header/Header';
+import About from './pages/about/About';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <Navbar/>
+    <Router>
+      <Navbar/>
+      <Routes>
+     <Route>
+      <Header/>
+      </Route>
+      <Route>
+      <About/>
+      </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Typed from "react-typed"
+import "./header.css"
+
+function Header() {
+  return (
+    <div className='header-wraper'>
+        <div className='main-info'>
+           <h1> front-end noob</h1>
+           <Typed
+           className='typed-text'
+           strings={["web design","web devolopment","Google ads","Facebook ads"]}
+           typeSpeed={50}
+           backSpeed={60}
+           loop/>
+           <a className='btn-main-offer'>contact me</a> 
+        </div>
+    </div>
+  )
+}
+export default Header
